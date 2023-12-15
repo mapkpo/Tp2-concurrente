283bc5251a789a9adb3dedd50fe8b28fce2dc4c9
Elly <elias.esparza.553@mi.unc.edu.ar>
2023-12-15 11:22:25 -0300

    Exportador #1

---
+++ src/main/Exportador.java
@@ -0,0 +1,31 @@
+package main;
+
+public class Exportador implements Runnable{
+    final Monitor monitor;
+    String threadName;
+
+    public Exportador(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    @Override
+    public void run() {
+        threadName = Thread.currentThread().getName();
+
+        while (true){
+            System.out.println(threadName + ": Buscando imagen para exportar.");
+            Imagen img = monitor.startexporte();
+
+            System.out.println(threadName + ": Iniciando exporte.");
+            //Simula tiempo de carga.
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException ex){
+                System.out.println(ex.getMessage());
+            }
+
+            monitor.finishexport(img);
+            System.out.println(threadName + ": Imagen exportada exitósamente.");
+        }
+    }
+}
